851e1e60b77630f69be86b4b71e8fbd432207d3b
miyakoshiaoi <103973945+miyakoshiaoi@users.noreply.github.com>
2023-02-09 12:52:52 +0900

    Update ex1-2.js

--- ex1-2.js
+++ ex1-2.js
@@ -1,2 +1,2 @@
-function imgchange1(){document.getElementById('nira').src='./image/nira1.png';}
-function imgchange2(){document.getElementById('nira').src='./image/nira2.png';}
+function imgchange1(){document.getElementById('nira').src='./image/nira1.PNG';}
+function imgchange2(){document.getElementById('nira').src='./image/nira2.PNG';}
